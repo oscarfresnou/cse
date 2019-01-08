f2b8c94a854d3270e9ba45433a70f31343d6646a
oscarfresnou <42384173+oscarfresnou@users.noreply.github.com>
2019-01-08 08:44:11 -0800

    semester 2 notes

---
+++ notes/semester 2 notes.py
@@ -0,0 +1,18 @@
+print("Hello World!")
+# My Name is Oscar Alejandro
+car = 5
+driving = True
+print("I have %s cars" % car)
+print("I have " +str(car) + " cars")
+
+age = input("How old are you?")
+print("haw haw you're %s" % age)
+
+colors = ["blue", "yellow", "orange", "green", "red"]
+colors.append("cyan")
+
+print(colors)
+
+colors.pop(0)
+
+print(colors)
