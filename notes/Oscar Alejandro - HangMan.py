c1331c2eec480a342c003b8aebeb89914bf73294
oscarfresnou <42384173+oscarfresnou@users.noreply.github.com>
2019-01-11 09:45:53 -0800

    hangman

--- notes/Oscar Alejandro - HangMan.py
+++ notes/Oscar Alejandro - HangMan.py
@@ -1,4 +1,4 @@
+import random
 word_bank = ["Big Chungus", "If you're reading this, it's to late", "T-pose", "Thanos", "Concerned", "Flex Tape",
              "That's a lot of damage!", "Freedom", "Fear", "Skins"]
-
-
+chosen_word = word_bank[random.randint(0,len(word_bank - 1)]
