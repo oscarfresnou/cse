a8f22a3412d844f6cf2e6231796bbe86607286e6
oscarfresnou <42384173+oscarfresnou@users.noreply.github.com>
2019-01-15 08:42:26 -0800

    hangman

--- notes/Oscar Alejandro - HangMan.py
+++ notes/Oscar Alejandro - HangMan.py
@@ -2,8 +2,5 @@ import random
 word_bank = ["Big Chungus", "Communism", "T-pose", "Thanos", "Concerned", "Flex Tape",
              "That's a lot of damage!", "Freedom", "Fear", "Skins"]
 chosen_word = word_bank[random.randint(0, len(word_bank - 1))]
-guesses_remaining = 12
+guesses = 12
 turns = 10
-while turns > 0:
-    failed = 0
-    
