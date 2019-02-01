f2623519a8867c74991890af505a15b8ced8fa0c
oscarfresnou <42384173+oscarfresnou@users.noreply.github.com>
2019-02-01 09:41:19 -0800

    World Dictionary

--- notes/Oscar Alejandro - World Dictionary.py
+++ notes/Oscar Alejandro - World Dictionary.py
@@ -1,6 +1,36 @@
 world_map = {
     'R19A': {
-        'NAME': "Wiebe's Classroom",
-        'DESCRIPTION': "This  is the classroom that you are in right now. It has 2 exits to the North"
+        'NAME': "Mr. Wiebe's room",
+        'DESCRIPTION': "This is the room that you are in.",
+        'PATHS': {
+            'NORTH': "PARKING_LOT"
+        }
+    },
+    'PARKING_LOT': {
+        'NAME': "A Parking Lot",
+        'DESCRIPTION': "There are a few cars parked here.",
+        'PATHS': {
+            'SOUTH': 'R19A'
+        }
     }
 }
+
+# Other variables
+current_node = ['R19A']
+directions = ["NORTH", "SOUTH", "EAST", "WEST", "UP", "DOWN"]
+playing = True
+
+# Controller
+while playing:
+    command = input(">_")
+    if command.lower() in ('q', 'quit', 'exit'):
+        playing = False
+    elif command.upper() in directions:
+        try:
+            room_name = current_node['PATHS'][command.upper()]
+            current_node = world_map[room_name]
+        except KeyError:
+            print("I can't go that way")
+        else:
+            print("command not recognized")
+
