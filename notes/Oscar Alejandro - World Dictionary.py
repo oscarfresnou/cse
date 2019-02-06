22686341e90974f5dd043d765c1ca9431486b798
oscarfresnou <42384173+oscarfresnou@users.noreply.github.com>
2019-02-06 08:41:54 -0800

    WORLD DICTIONARY

--- notes/Oscar Alejandro - World Dictionary.py
+++ notes/Oscar Alejandro - World Dictionary.py
@@ -10,19 +10,33 @@ world_map = {
         'NAME': "A Parking Lot",
         'DESCRIPTION': "There are a few cars parked here.",
         'PATHS': {
-            'SOUTH': 'R19A'
+            'SOUTH': 'R19A',
+            'NORTH': "PARKING_LOT_GATE"
         }
+    },
+    'PARKING_LOT_GATE': {
+        'NAME': "The Front Gate",
+        'DESCRIPTION': "The Gates are rusting, but the doors aren't locked.",
+        'PATH': {
+            'SOUTH': 'PARKING_LOT',
+            'NORTH': 'MIDDLE_OF_THE_ROAD'
+        }
+    },
+    'MIDDLE_OF_THE_ROAD': {
+        'NAME': "Road",
+        'DESCRIPTION': "There are puddles of green water and abandoned cars"
     }
 }
 
 # Other variables
-current_node = (world_map['R19A'])
+current_node = world_map['R19A']
 directions = ["NORTH", "SOUTH", "EAST", "WEST", "UP", "DOWN"]
 playing = True
 
 # Controller
 while playing:
-    command = input(">_")
+    print(current_node['NAME'])
+    command = input(">_").strip()
     if command.lower() in ('q', 'quit', 'exit'):
         playing = False
     elif command.upper() in directions:
@@ -31,6 +45,6 @@ while playing:
             current_node = world_map[room_name]
         except KeyError:
             print("I can't go that way")
-        else:
-            print("command not recognized")
+    else:
+        print("command not recognized")
 
