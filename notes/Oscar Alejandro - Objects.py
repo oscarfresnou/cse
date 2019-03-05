3a5fa2d0b0fe58fd0c248f5fce9f6ebc8518a46a
oscarfresnou <42384173+oscarfresnou@users.noreply.github.com>
2019-03-05 08:42:30 -0800

    object

--- notes/Oscar Alejandro - Objects.py
+++ notes/Oscar Alejandro - Objects.py
@@ -1,2 +1,10 @@
-class Lapotp(object):
+class Laptop(object):
     def __init__(self):
+        self.battery = 100
+        self.charger = True
+        self.mouse
+        self.key_board
+        self.power_button
+        self.volume_button
+        self.screen
+
