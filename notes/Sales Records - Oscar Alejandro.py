606d1b5f7961941f59a2f392106cdfa2e0559de7
oscarfresnou <42384173+oscarfresnou@users.noreply.github.com>
2019-05-13 08:43:15 -0700

    Sales Records

--- notes/Sales Records - Oscar Alejandro.py
+++ notes/Sales Records - Oscar Alejandro.py
@@ -1,2 +1,24 @@
 import csv
 
+with open("Sales Records.csv", 'r') as old_csv:
+    reader = csv.reader(old_csv)
+    print("Processing...")
+    fruit_total = 0
+    clothes_total = 0
+    for row in reader:
+        if row[0] == "Region":
+            continue
+        item = row[2]
+        profit_number = float(row[13])
+        cost_number = row[12]
+        revenue_number = row[11]
+
+        if "Fruits" == item:
+            fruit_total += profit_number
+    print("Total Fruit Profit", round(fruit_total, 2))
+
+    if "Clothes" == item:
+        clothes_total += profit_number
+print("Total Clothes Profit", round(clothes_total, 2))
+print("Finished")
+
--- notes/Validator - Oscar Alejandro.py
+++ notes/Validator - Oscar Alejandro.py
@@ -18,14 +18,50 @@ def validate(num: str):
 print(validate(test_num))
 
 
-def reverse(num: str):
-    print(num)
-    print(num[::-1])
+def check_second_digits(num):
+    length = len(num)
+    sum = [0]
+    for i in range(length-2,-1,-2):
+        number = eval(num[i])
+        number = number * (2)
+      if number > 9:
+        str_Number = str(number)
+        number = eval(str_Number[0]) + eval(str_Number[1])
+        sum += number
+    return sum
 
 
-def valid(num: str):
-    reversed_version = reverse(num)
-    for i in reversed_version:
-        ...
+def odd_digits(num):
+    length = len(num)
+    sum_odd = 0
+    for i in range(length-1,-1,-2):
+        num += eval(num[i])
+    return sum_odd
 
-    reverse("4696467524043400")
+
+def c_length(num):
+    length = len(num)
+    if num >= 13 and num <= 16:
+        if num [0] == "4" or num [0] == "5" or num [0] == "6" or (num [0] == "3" and num [1] == "7"):
+            return True
+    else:
+        return False
+
+
+def main():
+    filename = input("What is the name of your input file? ")
+    infile= open(filename,"r")
+    cc = (infile.readline().strip())
+    print(format("Card Number", "20s"), ("Valid / Invalid"))
+    print("------------------------------------")
+    while cc!= "EXIT":
+        even = check_second_digits(num)
+        odd = odd_digits(num)
+        c_len = c_length(num)
+        tot = even + odd
+
+        if c_len == True and tot % 10 == 0:
+            print(format(cc, "20s"), format("Valid", "20s"))
+        else:
+            print(format(cc, "20s"), format("Invalid", "20s"))
+        num = (infile.readline().strip())
