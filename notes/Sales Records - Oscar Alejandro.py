bf67ba423a8239a6f92dc8a8d00bb5a4fff7857f
oscarfresnou <42384173+oscarfresnou@users.noreply.github.com>
2019-05-14 08:45:06 -0700

    Sales Records

--- notes/Sales Records - Oscar Alejandro.py
+++ notes/Sales Records - Oscar Alejandro.py
@@ -5,6 +5,9 @@ with open("Sales Records.csv", 'r') as old_csv:
     print("Processing...")
     fruit_total = 0
     clothes_total = 0
+    Meat_total = 0
+    Beverages_total = 0
+    Office_Supplies_Total = 0
     for row in reader:
         if row[0] == "Region":
             continue
@@ -15,10 +18,17 @@ with open("Sales Records.csv", 'r') as old_csv:
 
         if "Fruits" == item:
             fruit_total += profit_number
+        if "Clothes" == item:
+            clothes_total += profit_number
+        if "Beverages" == item:
+            Beverages_total += profit_number
+        if "Meat" == item:
+            Meat_total += profit_number
+        if "Office Supplies" == item:
+            Office_Supplies_Total += profit_number
     print("Total Fruit Profit", round(fruit_total, 2))
-
-    if "Clothes" == item:
-        clothes_total += profit_number
-print("Total Clothes Profit", round(clothes_total, 2))
-print("Finished")
+    print("Total Clothes _Profit", round(clothes_total, 2))
+    print("Total Beverages Total", round(Beverages_total, 2))
+    print("Total Meat Total", round(Meat_total, 2))
+    print("Office Supplies Total", round(Office_Supplies_Total, 2))
 
